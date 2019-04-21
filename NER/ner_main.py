1dd6a20d7719c4e2bb8674c6f6c9d944494ff287
yuhaitao1994 <yuhaitaozmx@outlook.com>
2019-04-21 23:43:38 +0800

    commit

--- NER/ner_main.py
+++ NER/ner_main.py
@@ -149,10 +149,179 @@ class NerProcessor(DataProcessor):
             return lines
 
 
-def get_tfrecord():
+def convert_single_example(ex_index, example, label_list, max_seq_length, tokenizer, output_dir, mode):
+    """
+    将一个样本进行分析，然后将字转化为id, 标签转化为id,然后结构化到InputFeatures对象中
+    :param ex_index: index
+    :param example: 一个样本
+    :param label_list: 标签列表
+    :param max_seq_length:
+    :param tokenizer:
+    :param output_dir
+    :param mode:
+    :return:
+    """
+    label_map = {}
+    # 1表示从1开始对label进行index化
+    for (i, label) in enumerate(label_list, 1):
+        label_map[label] = i
+    # 保存label->index 的map
+    if not os.path.exists(os.path.join(output_dir, 'label2id.pkl')):
+        with codecs.open(os.path.join(output_dir, 'label2id.pkl'), 'wb') as w:
+            pickle.dump(label_map, w)
+
+    textlist = example.text.split(' ')
+    labellist = example.label.split(' ')
+    tokens = []
+    labels = []
+    for i, word in enumerate(textlist):
+        # 分词，如果是中文，就是分字,但是对于一些不在BERT的vocab.txt中得字符会被进行WordPice处理（例如中文的引号），可以将所有的分字操作替换为list(input)
+        token = tokenizer.tokenize(word)
+        tokens.extend(token)
+        label_1 = labellist[i]
+        for m in range(len(token)):
+            if m == 0:
+                labels.append(label_1)
+            else:  # 一般不会出现else
+                labels.append("X")
+    # tokens = tokenizer.tokenize(example.text)
+    # 序列截断
+    if len(tokens) >= max_seq_length - 1:
+        tokens = tokens[0:(max_seq_length - 2)]  # -2 的原因是因为序列需要加一个句首和句尾标志
+        labels = labels[0:(max_seq_length - 2)]
+    ntokens = []
+    segment_ids = []
+    label_ids = []
+    ntokens.append("[CLS]")  # 句子开始设置CLS 标志
+    segment_ids.append(0)
+    # append("O") or append("[CLS]") not sure!
+    # O OR CLS 没有任何影响，不过我觉得O 会减少标签个数,不过拒收和句尾使用不同的标志来标注，使用LCS 也没毛病
+    label_ids.append(label_map["[CLS]"])
+    for i, token in enumerate(tokens):
+        ntokens.append(token)
+        segment_ids.append(0)
+        label_ids.append(label_map[labels[i]])
+    ntokens.append("[SEP]")  # 句尾添加[SEP] 标志
+    segment_ids.append(0)
+    # append("O") or append("[SEP]") not sure!
+    label_ids.append(label_map["[SEP]"])
+    input_ids = tokenizer.convert_tokens_to_ids(
+        ntokens)  # 将序列中的字(ntokens)转化为ID形式
+    input_mask = [1] * len(input_ids)
+    # label_mask = [1] * len(input_ids)
+    # padding, 使用
+    while len(input_ids) < max_seq_length:
+        input_ids.append(0)
+        input_mask.append(0)
+        segment_ids.append(0)
+        # we don't concerned about it!
+        label_ids.append(0)
+        ntokens.append("**NULL**")
+        # label_mask.append(0)
+    # print(len(input_ids))
+    assert len(input_ids) == max_seq_length
+    assert len(input_mask) == max_seq_length
+    assert len(segment_ids) == max_seq_length
+    assert len(label_ids) == max_seq_length
+    # assert len(label_mask) == max_seq_length
+
+    # 打印部分样本数据信息
+    if ex_index < 5:
+        tf.logging.info("*** Example ***")
+        tf.logging.info("guid: %s" % (example.guid))
+        tf.logging.info("tokens: %s" % " ".join(
+            [tokenization.printable_text(x) for x in tokens]))
+        tf.logging.info("input_ids: %s" %
+                        " ".join([str(x) for x in input_ids]))
+        tf.logging.info("input_mask: %s" %
+                        " ".join([str(x) for x in input_mask]))
+        tf.logging.info("segment_ids: %s" %
+                        " ".join([str(x) for x in segment_ids]))
+        tf.logging.info("label_ids: %s" %
+                        " ".join([str(x) for x in label_ids]))
+        # tf.logging.info("label_mask: %s" % " ".join([str(x) for x in label_mask]))
+
+    # 结构化为一个类
+    feature = InputFeatures(
+        input_ids=input_ids,
+        input_mask=input_mask,
+        segment_ids=segment_ids,
+        label_ids=label_ids,
+        # label_mask = label_mask
+    )
+    # mode='test'的时候才有效
+    write_tokens(ntokens, output_dir, mode)
+    return feature
+
+
+def filed_based_convert_examples_to_features(
+        examples, label_list, max_seq_length, tokenizer, output_file, output_dir, mode=None):
+    """
+    将数据转化为TF_Record 结构，作为模型数据输入
+    :param examples:  样本
+    :param label_list:标签list
+    :param max_seq_length: 预先设定的最大序列长度
+    :param tokenizer: tokenizer 对象
+    :param output_file: tf.record 输出路径
+    :param mode:
+    :return:
+    """
+    writer = tf.python_io.TFRecordWriter(output_file)
+    # 遍历训练数据
+    for (ex_index, example) in enumerate(examples):
+        if ex_index % 5000 == 0:
+            tf.logging.info("Writing example %d of %d" %
+                            (ex_index, len(examples)))
+        # 对于每一个训练样本,
+        feature = convert_single_example(
+            ex_index, example, label_list, max_seq_length, tokenizer, output_dir, mode)
+
+        def create_int_feature(values):
+            f = tf.train.Feature(
+                int64_list=tf.train.Int64List(value=list(values)))
+            return f
+
+        features = collections.OrderedDict()
+        features["input_ids"] = create_int_feature(feature.input_ids)
+        features["input_mask"] = create_int_feature(feature.input_mask)
+        features["segment_ids"] = create_int_feature(feature.segment_ids)
+        features["label_ids"] = create_int_feature(feature.label_ids)
+        # features["label_mask"] = create_int_feature(feature.label_mask)
+        # tf.train.Example/Feature 是一种协议，方便序列化？？？
+        tf_example = tf.train.Example(
+            features=tf.train.Features(feature=features))
+        writer.write(tf_example.SerializeToString())
+
+
+def get_tfrecord(args, processor):
     """
     生成tf_record文件
     """
+    train_examples = processor.get_train_examples(args.data_dir)
+    eval_examples = processor.get_dev_examples(args.data_dir)
+    num_train_steps = int(
+        len(train_examples) * 1.0 / args.batch_size * args.num_train_epochs)
+    if num_train_steps < 1:
+        raise AttributeError('training data is so small...')
+    num_warmup_steps = int(num_train_steps * args.warmup_proportion)
+    tf.logging.info("***** Running training *****")
+    tf.logging.info("  Num examples = %d", len(train_examples))
+    tf.logging.info("  Batch size = %d", args.batch_size)
+    tf.logging.info("  Num steps = %d", num_train_steps)
+
+    tf.logging.info("***** Running evaluation *****")
+    tf.logging.info("  Num examples = %d", len(eval_examples))
+    tf.logging.info("  Batch size = %d", args.batch_size)
+
+    # 写入tfrecord
+    train_file = os.path.join(args.output_dir, "train.tf_record")
+    if not os.path.exists(train_file):
+        filed_based_convert_examples_to_features(
+            train_examples, label_list, args.max_seq_length, tokenizer, train_file, args.output_dir)
+    eval_file = os.path.join(args.output_dir, "eval.tf_record")
+    if not os.path.exists(eval_file):
+        filed_based_convert_examples_to_features(
+            eval_examples, label_list, args.max_seq_length, tokenizer, eval_file, args.output_dir)
 
 
 def train_and_eval(args):
@@ -168,6 +337,11 @@ def predict(args):
 
 
 if __name__ == '__main__':
+    """
+    开始执行
+    """
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.device_map
+    tf.logging.set_verbosity(tf.logging.INFO)
     args = get_args_parser()
     if True:
         param_str = '\n'.join(['%20s = %s' % (k, v)
@@ -175,8 +349,56 @@ if __name__ == '__main__':
         print('usage: %s\n%20s   %s\n%s\n%s\n' %
               (' '.join(sys.argv), 'ARG', 'VALUE', '_' * 50, param_str))
 
+    processors = {
+        "ner": NerProcessor
+    }
+    bert_config = modeling.BertConfig.from_json_file(args.bert_config_file)
+
+    if args.max_seq_length > bert_config.max_position_embeddings:
+        raise ValueError(
+            "Cannot use sequence length %d because the BERT model "
+            "was only trained up to sequence length %d" %
+            (args.max_seq_length, bert_config.max_position_embeddings))
+
+    # 在re train 的时候，才删除上一轮产出的文件，在predicted 的时候不做clean
+    if args.clean and args.do_train:
+        print('hahaha')
+        if os.path.exists(args.output_dir):
+            def del_file(path):
+                ls = os.listdir(path)
+                for i in ls:
+                    c_path = os.path.join(path, i)
+                    if os.path.isdir(c_path):
+                        del_file(c_path)
+                    else:
+                        os.remove(c_path)
+
+            try:
+                del_file(args.output_dir)
+            except Exception as e:
+                print(e)
+                print('pleace remove the files of output dir and data.conf')
+                exit(-1)
+
+    # check output dir exists
+    if not os.path.exists(args.output_dir):
+        os.mkdir(args.output_dir)
+    # 创建ner dataprocessor对象
+    processor = processors[args.ner](args.output_dir)
+    label_list = label_list = processor.get_labels()
+
+    tokenizer = tokenization.FullTokenizer(
+        vocab_file=args.vocab_file, do_lower_case=args.do_lower_case)
+
+    session_config = tf.ConfigProto(
+        log_device_placement=False,
+        inter_op_parallelism_threads=0,
+        intra_op_parallelism_threads=0,
+        allow_soft_placement=True)
+
     if args.do_record:
-        get_tfrecord()
+        # 生成train和eval的tfrecord
+        get_tfrecord(args=args, processor=processor)
     if args.do_train and args.do_eval:
         train_and_eval(args=args)
     if args.do_predict:
